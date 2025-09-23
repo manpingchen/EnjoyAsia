32a51e031516b3856f6afdf3fc75d2e7054a52a9
Man Ping Chen <manpingchen@gmail.com>
2025-09-23 08:57:05 +0100

    faq頁面完成

--- js/all.js
+++ js/all.js
@@ -114,3 +114,16 @@ articleLists.forEach((articleList) => {
     articleList.scrollBy({ left: cardWidth, behavior: "smooth" });
   });
 });
+
+document.querySelectorAll(".faq-question").forEach((button) => {
+  button.addEventListener("click", () => {
+    const answer = button.nextElementSibling;
+    button.classList.toggle("active");
+
+    if (button.classList.contains("active")) {
+      answer.style.maxHeight = answer.scrollHeight + "px";
+    } else {
+      answer.style.maxHeight = null;
+    }
+  });
+});
